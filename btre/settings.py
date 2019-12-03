cfa3d85b958c08a7eac087dd85368bddf59cf40b
Anna <anadejava@gmail.com>
2019-12-02 22:32:01 -0800

    Rebranding

--- btre/settings.py
+++ btre/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'i83smyipj!o9u#!mtc6g_j7(63pl9fmny*!pxf^fwgy+i^ov1b'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
